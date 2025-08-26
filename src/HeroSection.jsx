535c76c5ae0a33683c5b7cbec8742cb947b8b830
Olumide <oobayemi3@gmail.com>
2025-08-26 12:44:05 +0100

    changes

--- src/HeroSection.jsx
+++ src/HeroSection.jsx
@@ -6,12 +6,12 @@ const HeroSection = () => {
     <section id='home' >
         <div className=' mt-20 min-h-screen rounded-[20px] py-10 mx-4 bg-gradient-to-r from-[#feeefe] to-[#e5f2fe] h-full'>
 
-        <div className='flex flex-wrap items-center mx-4'>
+        <div className='flex flex-wrap items-center mx-4 gap-96'>
           <div className='px-4 ms-5  max-w-[470px] text-7xl leading-tight'>
             <h1 className='font-semibold text-5xl '>Tailwind CSS <br /> Template for <span className='font-bold'>Apps <br /> and Software</span></h1>
           </div>
 
-          <div>
+          <div className=''>
               <img src={hero} alt="" />
           </div>
         </div>
