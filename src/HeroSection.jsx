1da0ccb5306f2e34cb7a4cfeb47e1742bed31ec8
Olumide <oobayemi3@gmail.com>
2025-08-28 09:18:14 +0100

    updated

--- src/HeroSection.jsx
+++ src/HeroSection.jsx
@@ -6,17 +6,20 @@ const HeroSection = () => {
     <section id='home' >
         <div className=' mt-20 min-h-screen rounded-[20px] py-10 mx-4 bg-gradient-to-r from-[#feeefe] to-[#e5f2fe] h-full'>
 
-        <div className='flex flex-wrap items-center mx-4 gap-x-24'>
-          <div className='px-4 ms-5  leading-tight gap-x-30 max-w-xl'>
-            <h1 className='font-semibold text-5xl'>Tailwind CSS <br /> Template for <span className='font-bold'>Apps <br /> and Software</span></h1>
-            <p className='text-[#9699a6] text-base pe-5 font-semibold font-2xl'>
+        <div className='flex flex-wrap items-center  gap-x-24'>
+          <div className='flex px-4 ms-5  mx-auto leading-tight l max-w-xl'>
+            <h1 className='font-semibold text-5xl mb-9  leading-14'>Tailwind CSS <br /> Template for <span className='font-bold'>Apps <br /> and Software</span></h1>
+            <p className='text-[#9699a6] leading-8 font-sans font-normal px-4 font-4xl text-lg'>
               All in one Tailwind CSS site template for - App and Software sites. Comes with all essential components, clean design and everything you can expect from a complete app landing page.
             </p>
-          </div>
 
-          <div className='ms-16   '>
+
+            <div className='justify content-center'>
               <img src={hero} alt="" />
           </div>
+          </div>
+
+          
         </div>
 
 
