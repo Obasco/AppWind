f143a793ad2b119b9941a313360768d7f9e71420
Olumide <oobayemi3@gmail.com>
2025-09-05 02:28:35 +0100

    update

--- src/AwardsSection.jsx
+++ src/AwardsSection.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const AwardsSection = () => {
   return (
-    <section>
-      <h1>Our Recent Awards</h1>
+    <section className='py-10'>
+      <h1 className='font-bold text-3xl text-center'>Our Recent Awards</h1>
     </section>
   )
 }
--- src/HeroSection.jsx
+++ src/HeroSection.jsx
@@ -6,30 +6,27 @@ const HeroSection = () => {
     <section id='home'>
       <div className='mt-20 min-h-screen rounded-[20px] py-10 mx-4 bg-gradient-to-r from-[#feeefe] to-[#e5f2fe]'>
         
-        <div className='flex flex-wrap items-center gap-x-16'>
+        <div className='flex flex-col-reverse md:flex-row items-center md:items-start gap-10 md:gap-16'>
           
-         
-          <div className='px-4 ms-9 max-w-xl'>
-            <h1 className='font-semibold text-5xl mb-9 leading-[3.5rem]'>
+          {/* Text Section */}
+          <div className='px-4 md:ms-9 max-w-xl text-center md:text-left mt-14'>
+            <h1 className='font-semibold text-3xl sm:text-4xl md:text-5xl mb-6 md:mb-9 leading-tight md:leading-[3.5rem]'>
               Tailwind CSS <br /> Template for <span className='font-bold'>Apps <br /> and Software</span>
             </h1>
-            <p className='text-[#9699a6] leading-8 font-sans text-lg mb-9'>
+            <p className='text-[#9699a6] leading-7 md:leading-8 font-sans text-base sm:text-lg mb-6 md:mb-9'>
               All in one Tailwind CSS site template for - App and Software sites. Comes with all essential components, clean design and everything you can expect from a complete app landing page.
             </p>
 
             <div>
-              <button className='bg-violet-500 text-white px-12 rounded-2xl py-4'>Explore</button>
-
-
-
-
-
+              <button className='bg-violet-500 hover:bg-violet-600 transition text-white px-8 sm:px-12 rounded-2xl py-3 sm:py-4'>
+                Explore
+              </button>
             </div>
           </div>
 
-          {/* Hero - img */}
-          <div className='flex ms-24 py-10 rounded-2xl '>
-            <img src={hero} alt="Hero Illustration" className='max-w-sm' />
+          {/* Hero Image */}
+          <div className='flex justify-center md:justify-end py-3 md:py-10'>
+            <img src={hero} alt="Hero Illustration" className='ps-[8rem]'/>
           </div>
         </div>
 
